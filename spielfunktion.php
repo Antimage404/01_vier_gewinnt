ca1ba46501ced1ea13433ba0bcfca38a5ffd6751
ChristianNotheisen <55128332+ChristianNotheisen@users.noreply.github.com>
2024-10-18 15:06:09 +0200

    Update spielfunktion.php
    beinhaltet den Datenbankeintrag beim Setzen eines Kreises, überprüft die Siegbedingung
    
    nicht fertig

--- spielfunktion.php
+++ spielfunktion.php
@@ -6,18 +6,14 @@
     jeder Kreis braucht eine id mit der form columnid || rowid, z.B. 23
 
 */
-$column = htmlspecialchars($_POST['column']);  //id der column
-$user = htmlspecialchars($_POST['user']);
+$column = $_POST['column'];  //id der column
+$user = $_POST['user'];
 
-if(test_active_user == true) {
-    if(test_free_column == true) {
+if(test_active_user() == true) {
+    if(test_free_column() == true) {
         $zugnumnmer = get_max_zugnummer();
-        add_to_column();
-        setField($column);   
-    }
-    else{
-                //wenn spalte voll: neue Spalte soll ausgewählt werden
-
+        $rowcolumn = setField($column);   
+        add_to_column($rowcolumn);
     }
 }
 
@@ -25,7 +21,7 @@ if(test_active_user == true) {
 
 //überprüfen, ob der Spieler an der Reihe ist
 function test_active_user() {
-    if($_POST['active_user'] == §user) return true;
+    if($_POST['active_user'] == $user) return true;
     else return false;
 }
 
@@ -62,9 +58,7 @@ function test_free_column() {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
         $sql = "SELECT * FROM currentgame WHERE feld = $column ";
-        $stmt = $connection->prepare($sql);
-        $stmt->bind_param("s", $username);
-    
+        $stmt = $connection->prepare($sql);    
         $stmt->execute();
         $result= $stmt->get_result();
         $stmt->close();
@@ -80,7 +74,7 @@ function test_free_column() {
 }
 
 //fügt der Spalte eins für den entsprechenden Spieler hinzu
-function add_to_column() {
+function add_to_column($rowcolumn) {
     $connection = new mysqli("localhost", "root", "", "vier_gewinnt");
 
     if ($connection->connect_error) {
@@ -88,12 +82,11 @@ function add_to_column() {
     }
     
     
-    //überprüft, ob die angegebene Spalte bereits voll ist
     if ($_SERVER["REQUEST_METHOD"] == "POST") {        
-            $sqlInsert = "  INSERT INTO currentgame(zugnummer, user, feld)
-                            VALUES(:zugnr, :user, :column)";
-            $stmt = $connection->prepare($sqlInsert);
-            $stmt->execute();
+        $sqlInsert = "  INSERT INTO currentgame(zugnummer, user, feld)
+                        VALUES(:zugnr, :user, :rowcolumn)";
+        $stmt = $connection->prepare($sqlInsert);
+        $stmt->execute();
         $stmt->close();
     } else {
         echo "Ungültige Anforderung.";
@@ -118,12 +111,56 @@ function setField($column) {
         $stmt->close();
 
         $rowcolumn = $column || $row;
-
         //setze das Feld mit der id rowcolumn auf die farbe des aktiven Users  fehlt
-} else {
-    echo "Ungültige Anforderung.";
+
+        return $rowcolumn;
+} 
 }
 
+//überprüft, ob das Spiel für den aktiven Spieler zu Ende ist
+function check_win_condition() {
+    $connection = new mysqli("localhost", "root", "", "vier_gewinnt");
+
+
+    if ($connection->connect_error) {
+        die("Verbindung fehlgeschlagen: " . $connection->connect_error);
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+        //lädt alle Felder eines Spielers in ein array
+        $sql = "SELECT feld FROM currentgame WHERE user = $user ";
+        $stmt = $connection->prepare($sql);    
+        $stmt->execute();
+        $result= $stmt->fetch_row();
+        $fieldArray = array();
+        $counter = 0;
+        while(true) {
+            $fieldArray[$counter] = $result;
+            $counter ++;
+            $result= $stmt->fetch_row();
+            if ($result == null) break;
+        }
+        $stmt->close();
+    }
+}
+
+
+//erhöht den Score des Siegers um 1
+function assign_win($winner) {
+    $connection = new mysqli("localhost", "root", "", "vier_gewinnt");
+
+
+    if ($connection->connect_error) {
+        die("Verbindung fehlgeschlagen: " . $connection->connect_error);
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+        //lädt alle Felder eines Spielers in ein array
+        $sql = "UPDATE users SET score = score + 1  WHERE user = $winner ";
+        $stmt = $connection->prepare($sql);    
+        $stmt->execute();
+        $stmt->close();
+    }
 }
 
 ?>
