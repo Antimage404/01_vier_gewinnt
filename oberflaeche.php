f01ab4bd9c0d7ce95921836a618fd0abc15e9d75
Alex Tutubalin <alex.tutubalin@web.de>
2024-11-15 12:25:34 +0100

    Update oberflaeche.php
    Mit Logout Button

--- oberflaeche.php
+++ oberflaeche.php
@@ -22,16 +22,22 @@ $username = $_SESSION['username'];
         <h1>Vier Gewinnt</h1>
         <h3>Willkommen, <?php echo htmlspecialchars($username); ?>!</h3>
 
-        <!-- Buttons für die Spalten -->
-        <div class="button-container">
-            <button1 id="1">Spalte 1</button1>
-            <button1 id="2">Spalte 2</button1>
-            <button1 id="3">Spalte 3</button1>
-            <button1 id="4">Spalte 4</button1>
-            <button1 id="5">Spalte 5</button1>
-            <button1 id="6">Spalte 6</button1>
-            <button1 id="7">Spalte 7</button1>
-        </div>
+    <!-- Buttons für die Spalten und Funktionsaufruf der Spielfunktion pro Klick auf ein Button -->
+
+    <form  action="spielfunktion.php" method="POST">
+            <input type="text" hidden name='user' id='user' value = 'tester'>
+            <input  type="text"hidden name='active_user' id='active_user' value = 'tester'>
+
+            <div class="button-container" style="margin: 20px;">
+                <button type="submit" name="column"  id="column" value = "1" >Spalte 1</button1>       
+                <button type="submit" name="column"  id="column" value = "2" >Spalte 2</button1>
+                <button type="submit" name="column"  id="column" value = "3" >Spalte 3</button1>
+                <button type="submit" name="column"  id="column" value = "4" >Spalte 4</button1>
+                <button type="submit" name="column"  id="column" value = "5" >Spalte 5</button1>
+                <button type="submit" name="column"  id="column" value = "6" >Spalte 6</button1>
+                <button type="submit" name="column"  id="column" value = "7" >Spalte 7</button1>
+            </div>
+    </form>
 
         <!-- Spielfeld -->
         <div class="game-board">
@@ -43,6 +49,17 @@ $username = $_SESSION['username'];
             }
             ?>
         </div>
+
+        <!-- Logout Funktion -->
+        <form action="logout.php" method="POST" >
+            <div class="logout-button" style="padding: 35px;">
+            <button type="submit" id="logout" >Logout</button>        
+            </div>
+        </form>
+
     </div>
+
+    
+    
 </body>
 </html>
